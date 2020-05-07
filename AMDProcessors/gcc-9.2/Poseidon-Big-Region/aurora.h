07d6ee74a13a06b9891d865b157b956c2479146e
matheusvnm <sandro-matheus@hotmail.com>
2020-05-07 20:57:11 -0300

    ADD: Adding the time of write the file on Joker-1

--- AMDProcessors/gcc-9.2/Poseidon-Big-Region/aurora.h
+++ AMDProcessors/gcc-9.2/Poseidon-Big-Region/aurora.h
@@ -53,7 +53,7 @@ static int package_map[MAX_PACKAGES];
 char packname[MAX_PACKAGES][256];
 char tempfile[256];
 double initGlobalTime = 0.0;
-// write_file_threshold for Joker-1: XXXXX
+// write_file_threshold for Joker-1: 0.000156
 // write_file_threshold for Krakoa: XXXXX
 // write_file_threshold for MatheusPC: 0.001138
 double write_file_threshold=0.001138;
