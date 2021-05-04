f6368396e7978c130e2f534a165aae0fbf0bfe57
Matheus Marques <sandro-matheus@hotmail.com>
2021-05-04 15:22:57 -0300

    Update boost.sh

--- IntelProcessors/poseidon++/boost.sh
+++ IntelProcessors/poseidon++/boost.sh
@@ -10,15 +10,15 @@ do
         ipc=$(egrep "insn" out.txt | awk '{print $4}')
         #echo "ipc = $ipc"
         
-        if [ $(echo "$ipc>0.6"| bc) -eq 0 ] && [ $ACTUAL_STATE -eq $TURBO_ENABLED ] ;
+        if [ $(echo "$ipc>$IPC_TARGET"| bc) -eq 0 ] && [ $ACTUAL_STATE -eq $TURBO_ENABLED ] ;
         then
                 ACTUAL_STATE=$TURBO_DISABLED    
                 echo $TURBO_DISABLED > /sys/devices/system/cpu/cpufreq/boost
 
-        elif [ $(echo "$ipc<=0.6"| bc) -eq 0 ] && [ $ACTUAL_STATE -eq $TURBO_DISABLED ];
+        elif [ $(echo "$ipc<=$IPC_TARGET"| bc) -eq 0 ] && [ $ACTUAL_STATE -eq $TURBO_DISABLED ];
         then
                 ACTUAL_STATE=$TURBO_ENABLED
                 echo $TURBO_ENABLED > /sys/devices/system/cpu/cpufreq/boost
         fi
-        perf stat -e instructions,cycles -o out.txt sleep 5.0
+        perf stat -e instructions,cycles -o out.txt sleep $TIME_TARGET
 done
